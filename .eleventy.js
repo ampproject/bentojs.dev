fef6272a7ad7f1203f83ef1a4408e8b2f4dd9ac4
Marco Correa <marcoabcorrea@gmail.com>
2021-09-23 12:40:20 +0200

    merge to main

--- .eleventy.js
+++ .eleventy.js
@@ -13,9 +13,8 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.setDataDeepMerge(true);
 
-
   eleventyConfig.addPassthroughCopy('assets');
-  eleventyConfig.addWatchTarget('./dist/assets/css');
+  eleventyConfig.addWatchTarget('./assets/**/*.css');
 
   eleventyConfig.addPairedShortcode('tip', noOpShortCode);
 

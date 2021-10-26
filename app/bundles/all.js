7851dbff00a2e797ab0de61136ff2f8b8af35985
Sebastian Benz <sbenz@google.com>
2021-10-26 14:40:00 +0200

    fix lint script

--- app/bundles/all.js
+++ app/bundles/all.js
@@ -1,4 +1,4 @@
-import Header from "../components/Header";
+import Header from '../components/Header';
 
 window.addEventListener('load', () => {
   new Header(document.querySelector('.bd-header'));

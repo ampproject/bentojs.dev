6fd972af88e6eef445845265942ce7d7994e81ee
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-10-26 16:25:40 +0200

    ✨ Add style and interactivity to example widget

--- app/bundles/documentation.js
+++ app/bundles/documentation.js
@@ -1,4 +1,5 @@
 import DocumentationSidebar from "../components/DocumentationSidebar";
+import Example from "../components/Example";
 
 window.addEventListener('load', () => {
   new DocumentationSidebar(
@@ -7,4 +8,8 @@ window.addEventListener('load', () => {
   new DocumentationSidebar(
     document.getElementById('bd-toc'),
     document.getElementById('bd-button-toc'));
+
+  for ($example of document.querySelectorAll('.bd-example')) {
+    new Example($example);
+  }
 });
---
+++ app/components/Example.js
@@ -0,0 +1,33 @@
+class Example {
+  constructor($example) {
+    console.log('Example', $example);
+    this.$example = $example;
+    this.$button = {
+      code: $example.querySelector('button.--code'),
+      preview: $example.querySelector('button.--preview'),
+    }
+
+    this.$code = $example.querySelector('.bd-example-code');
+    this.$preview = $example.querySelector('.bd-example-preview');
+
+    this.$button.code.addEventListener('click', (e) => {
+      this.toggle(e, false);
+    })
+    this.$button.preview.addEventListener('click', (e) => {
+      this.toggle(e, true);
+    })
+  }
+
+  toggle(e, force) {
+    console.log('toggle!', force);
+
+    e.preventDefault();
+    this.$button.code.classList.toggle('--active', !force);
+    this.$button.preview.classList.toggle('--active', force);
+
+    this.$code.classList.toggle('--hidden', force);
+    this.$preview.classList.toggle('--hidden', !force);
+  }
+}
+
+export default Example;

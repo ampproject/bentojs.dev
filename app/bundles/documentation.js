2b77254640956ee4efd0147cc5e18a5e6b067972
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-10-28 23:26:53 +0200

    🚧 Draft usage of bento-sidebar for documentation

--- app/bundles/documentation.js
+++ app/bundles/documentation.js
@@ -2,10 +2,10 @@ import DocumentationSidebar from '../components/DocumentationSidebar';
 import Example from '../components/Example';
 
 window.addEventListener('load', () => {
-  new DocumentationSidebar(
-    document.getElementById('bd-components'),
-    document.getElementById('bd-button-components')
-  );
+  // new DocumentationSidebar(
+  //   document.getElementById('bd-components'),
+  //   document.getElementById('bd-button-components')
+  // );
   new DocumentationSidebar(
     document.getElementById('bd-toc'),
     document.getElementById('bd-button-toc')

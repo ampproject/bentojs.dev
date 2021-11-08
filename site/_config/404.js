352f8ee571c2f25ffdc6458589650d5a0a82ae4e
Marco Correa <marcoabcorrea@gmail.com>
2021-11-08 19:42:01 +0100

    Fixing path

--- site/_config/404.js
+++ site/_config/404.js
@@ -4,7 +4,7 @@ module.exports = {
   callbacks: {
     ready: function (err, bs) {
       bs.addMiddleware('*', (req, res) => {
-        const content_404 = fs.readFileSync('dist/404/index.html');
+        const content_404 = fs.readFileSync('dist/404.html');
 
         // Add 404 http status code in request header.
         res.writeHead(404, {'Content-Type': 'text/html; charset=UTF-8'});

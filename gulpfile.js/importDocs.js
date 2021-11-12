6e5fa61714a5a69508239d85e48ddfc589b4e98b
Sebastian Benz <sbenz@google.com>
2021-11-12 20:51:15 +0100

    clean up

--- gulpfile.js/importDocs.js
+++ gulpfile.js/importDocs.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS-IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 const fs = require('fs/promises');
 const path = require('path');
 const fg = require('fast-glob');
@@ -194,7 +178,7 @@ async function importComponents() {
         document.content = _injectHeroExamples(document.content, componentName);
 
         await fs.writeFile(
-          path.join(COMPONENTS_DEST, `${componentName}.md`),
+          path.join(COMPONENTS_DEST, `${fileName}.md`),
           document.stringify()
         );
 
--- gulpfile.js/importHeroExamples.js
+++ gulpfile.js/importHeroExamples.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2021 The AMP HTML Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS-IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 const fs = require('fs/promises');
 const path = require('path');
 const fg = require('fast-glob');
@@ -21,7 +5,6 @@ const chalk = require('chalk');
 
 const EXTENSIONS_SRC = path.resolve(__dirname, '..', 'amphtml/extensions');
 const EXAMPLES_DEST = path.resolve(__dirname, '..', 'assets/iframes/hero-examples');
-
 const PATH_COMPONENT_NAME_PATTERN = /amphtml\/extensions\/(.*?)\//gm
 
 async function importHeroExamples() {

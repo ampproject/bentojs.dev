4c8284f1935930a211af0e96e439cbac3e5fab6f
Marco Correa <marcoabcorrea@gmail.com>
2021-11-18 14:36:49 +0100

    Fix regex for usage

--- gulpfile.js/importDocs.js
+++ gulpfile.js/importDocs.js
@@ -132,8 +132,12 @@ function _rewriteCodeFenceShToBash(contents) {
 }
 
 function _rewriteUsage(contents, componentName) {
-  const usage = `<div class="bd-usage bd-card bd-card--light-sea-green"><p>Use ${componentName} as a web component or a React functional component:</p><a class="bd-button" href="#web-component">↓ Web Component</a><a class="bd-button" href="#preact%2Freact-component">↓ React / Preact</a></div>\n## Web Component`;
-  return contents.replace(/(\#\# Web Component)/gm, usage);
+  const usage = `<div class="bd-usage bd-card bd-card--light-sea-green">
+  <p>Use ${componentName} as a web component or a React functional component:</p>
+  <a class="bd-button" href="#web-component">↓ Web Component</a>
+  <a class="bd-button" href="#preact%2Freact-component">↓ React / Preact</a>
+</div>\n## Web Component`;
+  return contents.replace(/(\#\# Web Component$)/gm, usage);
 }
 
 function _parseComponentName(content) {

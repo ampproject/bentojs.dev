6cfd32ca48cfc54f352b257ffb058610221e33a5
Marco Correa <marcoabcorrea@gmail.com>
2021-12-08 13:18:27 +0100

    Refactoting copy logic to a more generic approach

--- app/bundles/get-started.js
+++
@@ -1,7 +0,0 @@
-import CopyToClipboard from '../components/CopyToClipboard';
-
-window.addEventListener('load', () => {
-  for (codeSnippet of document.querySelectorAll('pre')) {
-    new CopyToClipboard(codeSnippet);
-  }
-});

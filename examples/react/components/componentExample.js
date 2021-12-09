a9a9c634492c174083f18313e736251e99d06a8e
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-12-09 16:43:43 +0100

    ✨ Set up parallel next.js project to render component samples

---
+++ examples/react/components/componentExample.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function ComponentExample({ children, title }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+}
